6c64981961f73582869ac1b247e585da1928e799
rodgersa <ivorycoastdre42@gmail.com>
2017-09-13 14:39:03 -0400

    hello robot

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from gopigo import*
+import time 
+class Piggy(object):
+    def __init__(self):
+        print("HelLO, I'm your robot")
+        self.dance()
 
-class Piggy(object)
-      def__init__(self):
-
-    print("HelLO, I'm your robot")
-    self.dance()
-
-
-def dance(self):
-time.sleep(1.5)
-right_rot()
-time.sleep(.5)
-stop()
+    def dance(self):
+        time.sleep(1.5)
+        right_rot()
+        time.sleep(.5)
+        stop()
 
 
 
 ###### My App
 
-mra = Piggy()
+dre = Piggy()
