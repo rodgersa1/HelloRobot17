be6bc5ea96d5d83a1b92e79a12dbe09596aef72b
rodgersa <ivorycoastdre42@gmail.com>
2017-09-13 14:33:28 -0400

    hello robot

---
+++ main.py
@@ -0,0 +1,20 @@
+from gopigo import*
+
+class Piggy(object)
+      def__init__(self):
+
+    print("HelLO, I'm your robot")
+    self.dance()
+
+
+def dance(self):
+time.sleep(1.5)
+right_rot()
+time.sleep(.5)
+stop()
+
+
+
+###### My App
+
+mra = Piggy()
